41d0cc87cb13f670a21eb26d7db0b727d8c16577
Rodrigo Antón Moreno <rodri.anton@gmail.com>
2025-05-11 11:21:29 +0200

    feat(tbot): review all the library

--- regadero/lib/telegram_bot.py
+++ regadero/lib/telegram_bot.py
@@ -21,6 +21,10 @@ class TelegramBot():
     first_name = None
     id = None
 
+    timeout = 90
+
+    last_update_date = None
+
     def __init__(self, token, chat_id=None):
 
         self.base_url = f"https://api.telegram.org/bot{token}"
@@ -33,22 +37,25 @@ class TelegramBot():
 
         (self.username, self.first_name, self.id) = self.get_me()
 
+        self.logger.info(f"bot initialized with name: {self.first_name} "
+                         f"- username: {self.username}")
+
 
     def _do_get(self, path):
         # OSError: -202 --> no wlan connected
         self.logger.info(f"doing get to {path}")
-        resp = requests.get(f"{self.base_url}/{path}")
+        resp = requests.get(f"{self.base_url}/{path}", timeout=self.timeout)
         if resp.status_code != 200:
             self.logger.error(f"Error doing get: {resp.content}")
             return
         return resp.json().get('result')
 
-    def _do_post(self, path, data=None, files=None):
+    def _do_post(self, path, data=None):
+
         self.logger.info(f"doing post to {path} - {data}")
         resp = requests.post(f"{self.base_url}/{path}",
                              json=data,
-                             #  files=files,  # this is for normal python
-                             headers=self.headers)
+                             headers=self.headers, timeout=self.timeout)
         if resp.status_code != 200:
             self.logger.error(f"Error doing post: {resp.content}")
             return False
@@ -56,16 +63,41 @@ class TelegramBot():
 
     def get_me(self):
         data = self._do_get('getMe')
-        self.logger.info(f"bot {data['first_name']} started successfull")
-        return data['username'], data['first_name'], data['id']
+        return '@' + data['username'], data['first_name'], data['id']
 
     def get_updates(self):
-        # we should grab the last data here
+        # we should grab the last date here
+        updates = self._do_get('getUpdates')
+        if len(updates) > 0:
+            self.last_update_date = updates[-1]['message']['date']
+            self.logger.info(f"last date is now {self.last_update_date}")
         return self._do_get('getUpdates')
+
+    def get_refered_updates(self):
+        "get messates containin the bot username in its text"
+        return [msg for msg in self.get_updates() if self.username in msg.get('text', '')]
+
+    def get_last_update(self):
+
+        self.logger.info(f"getting last update from chat {self.chat_id}")
+
+        updates = self.get_updates()
+
+        while len(updates > 0):
+            update = updates.pop()
+            if not 'message' in update:
+                self.logger.error("no message in last update!!")
+                return None
+
+            self.logger.info(f"last update from {update['message']['from']['first_name']} "
+                             f"on chat {update['message']['chat']['title']}")
+
+        return update
+
 
     def ok_to_last_message(self):
 
-        updates = self.get_updates()
+        updates = self.get_refered_updates()
 
         if len(updates) == 0:
             return
@@ -90,19 +122,3 @@ class TelegramBot():
             "disable_notification": not notify
         }
         return self._do_post('sendMessage', data)
-
-    def send_image(self, image_path, message=''):
-
-        # this is for normal python
-        # files = {
-        #     "photo": open(image_path, 'rb')
-        # }
-
-        data = {
-           "chat_id": self.chat_id,
-            "caption": message,
-            "disable_notification": False,
-            "photo": open(image_path, 'rb')
-        }
-        return self._do_post(f'sendPhoto?chat_id={self.chat_id}',
-                             data)  #, files) --> this is for normal python
