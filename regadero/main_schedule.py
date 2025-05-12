5e9e49f848d34b2a98b20f6ba3a50e8f295b4151
Rodrigo Antón Moreno <rodri.anton@gmail.com>
2025-05-12 18:42:16 +0200

    feat(main_schedule): use json_data for settings

--- regadero/main_schedule.py
+++ regadero/main_schedule.py
@@ -1,6 +1,5 @@
 import ntptime
 
-from json import loads as json_loads
 from machine import RTC, Pin
 
 from logger import Logger
@@ -8,9 +7,9 @@ from wifi_manager import configure_wifi
 from gpio_manager_small import blink
 from telegram_bot import TelegramBot
 
-from utils import datetime
+from utils import datetime, json_data
 
-SETTINGS = json_loads(open('settings.json', 'r').read())
+settings = json_data(open('settings.json', 'r').read())
 
 logger = Logger()
 
@@ -18,7 +17,7 @@ logger.info("configuring onboard led")
 bled = Pin(2, Pin.OUT, drive=Pin.DRIVE_0)
 
 
-wlan = configure_wifi(SETTINGS['wifi'])
+wlan = configure_wifi(settings('wifi'))
 if wlan.isconnected():
     blink(bled)
 
@@ -35,9 +34,10 @@ rtc.datetime((Y, M, D, WD, h + 2, m, s, ss))
 logger.info(f"  > time adjusted is {datetime.datetime()}")
 
 logger.info("Initializing Telegram bot")
-tbot = TelegramBot(SETTINGS['telegram']['token'],
+tbot = TelegramBot(settings('telegram.token'),
+                   settings('telegram.chat_id'))
                 #    SETTINGS['telegram']['group_chat_id'])
-                   SETTINGS['telegram']['chat_id'])
+
 if tbot:
     blink(bled)
 
